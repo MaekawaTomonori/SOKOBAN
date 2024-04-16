2333a01acfa6ce60ff019b00b5e74e55967dd4e3
Apocalypse <k023g0063@g.neec.ac.jp>
2024-04-16 10:09:22 +0900

    Playerの移動

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
 
-    private int[] map = new int[]{0,0,1,0,0};
+    private int[] map = new int[] { 0, 0, 1, 0, 0 };
 
     // Start is called before the first frame update
-    void Start() {
-        for (int i = 0; i < map.Length; ++i) {
-            Debug.Log(map[i] + ",");
-        }
+    void Start()
+    {
+        PrintArray();
     }
 
     // Update is called once per frame
     void Update() {
+        int playerIndex = getPlayerIndex();
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            MoveNumber(1, playerIndex, playerIndex + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            MoveNumber(1, playerIndex, playerIndex - 1);
+        }
+    }
 
+    void PrintArray() {
+        string output = "";
+        for (int i = 0; i < map.Length; ++i)
+        {
+            output += map[i].ToString() + ", ";
+        }
+
+        Debug.Log(output);
+
+    }
+
+    int getPlayerIndex()
+    {
+        int playerIndex = -1;
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i] == 1)
+            {
+                playerIndex = i;
+                break;
+            }
+        }
+        return playerIndex;
+    }
+
+    bool MoveNumber(int objectiveNum, int moveFrom, int moveTo)
+    {
+        if (moveTo < 0 || moveTo > map.Length) { return false; }
+        map[moveTo] = objectiveNum;
+        map[moveFrom] = map[moveTo];
+        return true;
     }
 }
